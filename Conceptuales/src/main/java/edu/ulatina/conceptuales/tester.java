f425fa9fcbc6337daea67b6c6bd13b30c2fe5046
dugarte@pernix-solutions.com <deividauq93@hotmail.com>
2018-11-27 20:23:24 -0600

    init with mvn

--- Conceptuales/src/main/java/edu/ulatina/conceptuales/tester.java
+++ Conceptuales/src/main/java/edu/ulatina/conceptuales/tester.java
@@ -3,19 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package conceptual;
+package edu.ulatina.conceptuales;
 
 /**
  *
- * @author deivi
+ * @author blaken
  */
-public class Conceptual {
-
-    /**
-     * @param args the command line arguments
-     */
+public class tester {
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola mundo");
     }
-    
 }
