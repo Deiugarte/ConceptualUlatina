fee01e0a959307cc4a8f558e541177107949c36b
dugarte@pernix-solutions.com <deividauq93@hotmail.com>
2018-11-27 21:13:59 -0600

    jenkins test

--- Conceptuales/src/main/java/edu/ulatina/conceptuales/tester.java
+++ Conceptuales/src/main/java/edu/ulatina/conceptuales/tester.java
@@ -11,6 +11,6 @@ package edu.ulatina.conceptuales;
  */
 public class tester {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundo 2");
     }
 }
